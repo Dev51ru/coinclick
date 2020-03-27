c0c29334a90cf5a29ab1bfe8992065793f4a25ee
Dev51 <33731541+Deveveloper@users.noreply.github.com>
2020-03-27 17:19:00 +0300

    Coin
    Click

--- offline.js
+++ offline.js
@@ -1,30 +1,24 @@
 ﻿{
-	"version": 1585317282,
+	"version": 1585318701,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/cursor-sheet0.png",
-		"images/buttonacursor-sheet0.png",
-		"images/buttonpcursor-sheet0.png",
-		"images/videocard-sheet0.png",
-		"images/sprite2-sheet0.png",
-		"images/sprite-sheet0.png",
-		"images/sprite3-sheet0.png",
-		"images/stoyka_videocard-sheet0.png",
-		"images/supercomp-sheet0.png",
-		"images/server-sheet0.png",
 		"images/click-sheet0.png",
+		"images/top-sheet0.png",
+		"images/perevod-sheet0.png",
 		"images/shop-sheet0.png",
-		"images/sprite5-sheet0.png",
-		"images/sprite4-sheet0.png",
+		"images/strelka-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/video-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
 		"loading-logo.png",
-		"xd_connection.js"
+		"jquery.gritter.js",
+		"jquery.gritter.css"
 	]
 }
